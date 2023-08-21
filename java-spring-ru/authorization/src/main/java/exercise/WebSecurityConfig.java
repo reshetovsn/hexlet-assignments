8efcc4480b15b77e47d3b2e076a9736b11ac7c90
@hexlet/cli <support@hexlet.io>
2023-08-21 11:22:15 +0300

    submit java-spring-ru/authorization

---
+++ java-spring-ru/authorization/src/main/java/exercise/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package exercise;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.DELETE;
+
+import exercise.model.UserRole;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // BEGIN
+        http.csrf().disable();
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/").permitAll()
+                .requestMatchers(POST, "/users").permitAll()
+                .requestMatchers(GET, "/users/**").hasAnyAuthority(UserRole.USER.name(), UserRole.ADMIN.name())
+                .requestMatchers(DELETE, "/users/**").hasAuthority(UserRole.ADMIN.name())
+                )
+                .httpBasic();
+        return http.build();
+        // END
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
