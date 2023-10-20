12912229735054fc8944e997b978c1d869dd4c5d
@hexlet/cli <support@hexlet.io>
2023-10-20 13:34:23 +0300

    submit java-spring-ru/configuration

---
+++ java-spring-ru/configuration/src/main/java/exercise/Application.java
@@ -0,0 +1,65 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import  org.springframework.beans.factory.annotation.Autowired;
+
+import exercise.model.User;
+import exercise.component.UserProperties;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    // Все пользователи
+    private List<User> users = Data.getUsers();
+
+    // BEGIN
+    @Autowired
+    private UserProperties adminsInfo;
+
+    @GetMapping("/admins")
+    @ResponseStatus(HttpStatus.OK)
+    public List<String> showAdmins() {
+        List<String> adminsEmails = adminsInfo.getAdmins();
+
+        return users.stream()
+                .filter(u -> {
+                    for (String email: adminsEmails) {
+                        if (u.getEmail().equals(email)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .map(User::getName)
+                .sorted()
+                .toList();
+    }
+    // END
+
+    @GetMapping("/users")
+    public List<User> index() {
+        return users;
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> show(@PathVariable Long id) {
+        var user = users.stream()
+            .filter(u -> u.getId() == id)
+            .findFirst();
+        return user;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
